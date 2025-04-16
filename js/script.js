3d67e378981ada4b5a83fca1edabc50505bd7479
mkktetsu <mkktetsu@gmail.com>
2025-04-17 03:30:42 +0900

    gsap

--- js/script.js
+++ js/script.js
@@ -1,1 +1,48 @@
-$(function () {});
+$(function () {
+//猫の動き
+gsap.fromTo(
+    //ターゲット
+    '.move-in-cat',
+    //初めの状態
+    {
+        opacity: 1,
+        x: 0,
+        y: -150
+    }
+    ,
+    //最後の状態
+    {
+        opacity: 0,
+        x:-50,
+        y:30,
+        scrollTrigger:{
+            trigger: '.move-in-cat',
+            start: 'bottom center',
+        }
+    }
+
+);
+
+
+ // 2匹目（下に滑る猫）
+gsap.fromTo(
+    '.move-out-cat', // ←ここに新しいクラス
+    {
+    opacity: 0,
+    x: 100,
+    y: -30,
+    },
+    {
+    opacity: 1,
+    x: -300,
+    y: 100,
+    duration: 1.5,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: '.move-out-cat', // ←トリガーもそれに合わせて
+        start: 'top center',
+    }
+    }
+);
+
+});
